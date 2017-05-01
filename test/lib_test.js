4ca493acce51e6d5846f18aa80af611820ed583b
Taka Okunishi <okunishinishi@gmail.com>
2017-05-01 14:33:03 +0900

    Version incremented to 1.0.9

---
+++ test/lib_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for lib.
+ * Runs with mocha.
+ */
+'use strict'
+
+const lib = require('../lib/lib.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('lib', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Lib', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
