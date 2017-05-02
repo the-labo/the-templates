6f0989faf6a4db09f78435797e7592053d0b84c0
Taka Okunishi <okunishinishi@gmail.com>
2017-05-02 21:24:48 +0900

    Version incremented to 1.0.10

---
+++ test/dir_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for dir.
+ * Runs with mocha.
+ */
+'use strict'
+
+const dir = require('../lib/dir.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('dir', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Dir', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
