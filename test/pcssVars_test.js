ba582c713316ea70e528fdce05ab5e962cd40fee
Taka Okunishi <okunishinishi@gmail.com>
2017-05-30 13:55:43 +0900

    Version incremented to 1.1.5

---
+++ test/pcssVars_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for pcssVars.
+ * Runs with mocha.
+ */
+'use strict'
+
+const pcssVars = require('../lib/pcssVars.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('pcss-vars', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Pcss vars', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
