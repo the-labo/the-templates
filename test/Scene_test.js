c1159e6d2919f4dec8b6ab62783b71bd07f65b25
okunishinishi <okunishinishi@gmail.com>
2018-01-16 11:32:54 +0900

    Version incremented to 2.3.1

---
+++ test/Scene_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for scene.
+ * Runs with mocha.
+ */
+'use strict'
+
+const scene = require('../lib/Scene.js')
+const assert = require('assert')
+
+describe('scene', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Scene', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
