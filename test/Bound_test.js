654c7a9b485ad3038cdb0106471d1123c90c2938
okunishinishi <okunishinishi@gmail.com>
2018-01-21 00:10:16 +0900

    Version incremented to 2.4.4

---
+++ test/Bound_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for bound.
+ * Runs with mocha.
+ */
+'use strict'
+
+const bound = require('../lib/Bound.js')
+const assert = require('assert')
+
+describe('bound', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Bound', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
