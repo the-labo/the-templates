5d3327ab1f3e5899e019321b433e31c511ed5c35
okunishinishi <okunishinishi@gmail.com>
2018-01-31 08:53:04 +0900

    Version incremented to 2.5.4

---
+++ test/Resources_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for resources.
+ * Runs with mocha.
+ */
+'use strict'
+
+const resources = require('../lib/Resources.js')
+const assert = require('assert')
+
+describe('resources', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Resources', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
