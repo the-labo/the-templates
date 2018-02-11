d6ab3e52f65b7100bedf90e9a3b8ec13f7a9e3f6
okunishinishi <okunishinishi@gmail.com>
2018-02-11 17:33:21 +0900

    Version incremented to 2.5.8

---
+++ test/Stateful_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for stateful.
+ * Runs with mocha.
+ */
+'use strict'
+
+const stateful = require('../lib/Stateful.js')
+const assert = require('assert')
+
+describe('stateful', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Stateful', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
