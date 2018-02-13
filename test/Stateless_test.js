4853de456e1fee7354722bfff18d208c16ecbca3
okunishinishi <okunishinishi@gmail.com>
2018-02-13 15:40:22 +0900

    Version incremented to 2.5.9

---
+++ test/Stateless_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for stateless.
+ * Runs with mocha.
+ */
+'use strict'
+
+const stateless = require('../lib/Stateless.js')
+const assert = require('assert')
+
+describe('stateless', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Stateless', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
