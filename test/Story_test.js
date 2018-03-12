a2a125f604ae3d3a28c5803abc37e7e74e321192
okunishinishi <okunishinishi@gmail.com>
2018-03-12 13:56:41 +0900

    Version incremented to 3.1.7

---
+++ test/Story_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for story.
+ * Runs with mocha.
+ */
+'use strict'
+
+const story = require('../lib/Story.js')
+const assert = require('assert')
+
+describe('story', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Story', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
