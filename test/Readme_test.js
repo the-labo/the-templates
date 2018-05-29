9626d29c611bb9b3ec56267f7f23fcbe2e0aa002
okunishinishi <okunishinishi@gmail.com>
2018-05-29 20:28:32 +0900

    Version incremented to 3.2.2

---
+++ test/Readme_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for readme.
+ * Runs with mocha.
+ */
+'use strict'
+
+const readme = require('../lib/Readme.js')
+const assert = require('assert')
+
+describe('readme', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Readme', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
