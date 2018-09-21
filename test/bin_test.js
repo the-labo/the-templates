dde96a00cc99a2d81f97de8884cc79845d2da1cd
okunishinishi <okunishinishi@gmail.com>
2018-09-21 21:40:42 +0900

    Version incremented to 4.1.1

---
+++ test/bin_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for bin.
+ * Runs with mocha.
+ */
+'use strict'
+
+const bin = require('../lib/bin.js')
+const assert = require('assert')
+
+describe('bin', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Bin', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
