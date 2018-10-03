b637285230facd4b3fa67320f5424c0b7d2ac4ae
okunishinishi <okunishinishi@gmail.com>
2018-10-03 09:49:26 +0900

    Version incremented to 5.0.2

---
+++ test/Stream_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for stream.
+ * Runs with mocha.
+ */
+'use strict'
+
+const stream = require('../lib/Stream.js')
+const assert = require('assert')
+
+describe('stream', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Stream', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
